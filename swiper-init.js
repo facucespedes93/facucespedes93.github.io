b265387a2fa6e0a96038a8471ff43650e2d0e154
Facundo Cespedes <facucespedes93@gmail.com>
2025-05-12 19:03:31 +1200

    new montserrat font and updates to swiper function

--- swiper-init.js
+++ swiper-init.js
@@ -1,8 +1,23 @@
-var swiper = new Swiper(".mySwiper", {
-    loop: true,
+
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".mySwiper", {
+    loop: false,
     spaceBetween: 0,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    // Breakpoints for smaller device
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 3,
+        }
+    }
+    });
 });
